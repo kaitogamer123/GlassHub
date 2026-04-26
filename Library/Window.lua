d78c1622a347add701ffde3fae3575ae5a96b604
kaitogamer123 <140911276+kaitogamer123@users.noreply.github.com>
2026-04-26 07:22:04 +0700

    Create Window.lua

---
+++ Library/Window.lua
@@ -0,0 +1,16 @@
+local UserInputService = game:GetService("UserInputService")
+local TweenService = game:GetService("TweenService")
+
+local Window = {}
+
+function Window.CreateWindow(hubName)
+    -- ВСТАВЬ СЮДА ТВОЙ КОД CreateWindow
+    -- БЕЗ:
+    -- function Library:CreateWindow(...)
+    -- и БЕЗ return Library
+
+    -- В КОНЦЕ:
+    return WindowLogic
+end
+
+return Window
