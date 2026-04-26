d3c448f63b885e7f59da6c0a8acac0ac0ff5c032
kaitogamer123 <140911276+kaitogamer123@users.noreply.github.com>
2026-04-26 19:03:30 +0700

    Update InGameAutoFarm.lua

--- GlassScripts/InGameAutoFarm.lua
+++ GlassScripts/InGameAutoFarm.lua
@@ -1,15 +1,28 @@
-
 -- Файл: GlassScripts/InGameAutoFarm.lua
-return function(v)
-    local ReplicatedStorage = game:GetService("ReplicatedStorage")
-    local AutoFarmCmds = require(ReplicatedStorage.Library.Client.AutoFarmCmds)
-    local Library = ReplicatedStorage:WaitForChild("Library")
-    
-    if v then
-        pcall(function() AutoFarmCmds.Enable() end)
-        print("🧊 GlassHub: Отправлена команда ENABLE")
-    else
-        pcall(function() AutoFarmCmds.Disable() end)
-        print("🧊 GlassHub: Отправлена команда DISABLE")
-    end
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Library = ReplicatedStorage:WaitForChild("Library")
+local AutoFarmCmds = require(Library.Client.AutoFarmCmds)
+
+local Functions = {}
+
+-- Функция включения
+Functions.Enable = function()
+    pcall(function()
+        if not AutoFarmCmds.IsEnabled() then
+            AutoFarmCmds.Enable()
+            print("🧊 GlassHub: Официальный фарм ВКЛЮЧЕН")
+        end
+    end)
 end
+
+-- Функция выключения
+Functions.Disable = function()
+    pcall(function()
+        if AutoFarmCmds.IsEnabled() then
+            AutoFarmCmds.Disable()
+            print("🧊 GlassHub: Официальный фарм ВЫКЛЮЧЕН")
+        end
+    end)
+end
+
+return Functions
