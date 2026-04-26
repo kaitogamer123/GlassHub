35e5a6664e965cfa7e95d6a9f5f094eb81c04285
kaitogamer123 <140911276+kaitogamer123@users.noreply.github.com>
2026-04-26 07:23:04 +0700

    Create Elements.lua

---
+++ Library/Elements.lua
@@ -0,0 +1,13 @@
+local TweenService = game:GetService("TweenService")
+
+local Elements = {}
+
+function Elements.AddToggle(parent, side, text, callback)
+    -- код Toggle
+end
+
+function Elements.AddButton(parent, side, text, callback)
+    -- код Button
+end
+
+return Elements
