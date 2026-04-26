1aa094d77629ec3bd61724d6f8158099d87475b3
kaitogamer123 <140911276+kaitogamer123@users.noreply.github.com>
2026-04-26 20:36:15 +0700

    Update InGameAutoFarm.lua

--- GlassScripts/InGameAutoFarm.lua
+++ GlassScripts/InGameAutoFarm.lua
@@ -1,20 +1,29 @@
--- Файл: GlassScripts/InGameAutoFarm.lua
-return function()
-    local AutoFarmCmds = require(game:GetService("ReplicatedStorage").Library.Client.AutoFarmCmds)
 
-    task.spawn(function()
-        while true do
-            -- Используем getgenv, чтобы кнопка из лоадера (выше) работала
-            if getgenv().AutoFarmEnabled then
-                if not AutoFarmCmds.IsEnabled() then
-                    pcall(function() AutoFarmCmds.Enable() end)
-                end
-            else
-                if AutoFarmCmds.IsEnabled() then
-                    pcall(function() AutoFarmCmds.Disable() end)
-                end
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Library = ReplicatedStorage:WaitForChild("Library")
+local AutoFarmCmds = require(Library.Client.AutoFarmCmds)
+
+_G.AutoFarmEnabled = false
+
+task.spawn(function()
+    while true do
+        if _G.AutoFarmEnabled then
+            -- Если автофарм еще не включен - включаем его официально
+            if not AutoFarmCmds.IsEnabled() then
+                pcall(function()
+                    AutoFarmCmds.Enable()
+                end)
             end
-            task.wait(1) 
+        else
+            -- Если ты выключил чит - выключаем и автофарм игры
+            if AutoFarmCmds.IsEnabled() then
+                pcall(function()
+                    AutoFarmCmds.Disable()
+                end)
+            end
         end
-    end)
-end
+        task.wait(1) -- Проверяем статус раз в секунду
+    end
+end)
+
