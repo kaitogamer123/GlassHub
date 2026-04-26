3b5e3d67ecfefddabcfeb5cfb3e21e8b5231d6dd
kaitogamer123 <140911276+kaitogamer123@users.noreply.github.com>
2026-04-26 07:22:29 +0700

    Create Tabs.lua

---
+++ Library/Tabs.lua
@@ -0,0 +1,10 @@
+local TweenService = game:GetService("TweenService")
+
+local Tabs = {}
+
+function Tabs.CreateTab(WindowContext, name, emoji)
+    -- логика вкладок (Pages, Sidebar и т.п.)
+    return TabLogic
+end
+
+return Tabs
