1176eb7cbe30e1bce45bfbb204a507317ac237b9
kaitogamer123 <140911276+kaitogamer123@users.noreply.github.com>
2026-04-28 01:55:33 +0700

    Update EasterManager.lua

--- GlassScripts/EasterManager.lua
+++ GlassScripts/EasterManager.lua
@@ -17,28 +17,38 @@ return function()
             if getgenv().AutoEasterManager then
                 pcall(function()
                     local inv = Save.Get().Inventory.Misc or {}
-                    
-                    -- Собираем ID апгрейда (Например: Easter2026Egg8 + Luck)
                     local eggId = getgenv().SelectedEasterEgg or "Easter2026Egg8"
-                    local track = getgenv().SelectedEasterUpgrade or "Luck"
-                    local upgradeId = eggId .. track
-
-                    -- ШАГ 1: Покупаем выбранный апгрейд
-                    if track ~= "None" then
-                        local bought
-                        repeat
-                            bought = UpgradeRemote:InvokeServer(upgradeId)
-                            task.wait(0.1)
-                        until not bought or not getgenv().AutoEasterManager
+                    
+                    -- ШАГ 1: АПГРЕЙДЫ (Цикл по всем включенным типам)
+                    -- Ожидаем таблицу вида {Luck = true, ShinyLuck = false, ...}
+                    local upgrades = getgenv().SelectedEasterUpgrades or {}
+                    
+                    for track, enabled in pairs(upgrades) do
+                        if enabled then
+                            local upgradeId = eggId .. track
+                            -- Покупаем уровни, пока хватает валюты
+                            local bought
+                            repeat
+                                bought = UpgradeRemote:InvokeServer(upgradeId)
+                                if bought then task.wait(0.1) end
+                            until not bought or not getgenv().AutoEasterManager
+                        end
                     end
 
-                    -- ШАГ 2: Машина удачи
-                    local boostType = getgenv().SelectedEasterLuck or "Huge"
-                    if boostType ~= "None" then
-                        for _, item in pairs(inv) do
-                            local internalKey = tokenMapping[item.id]
-                            if internalKey and (item._am or 0) > 5000 then
-                                Net.Invoke("Easter2026ChanceMachine_AddTime", boostType, internalKey, item._am)
+                    -- ШАГ 2: МАШИНА УДАЧИ (Цикл по всем включенным бустам)
+                    -- Ожидаем таблицу вида {Huge = true, Titanic = true, ...}
+                    local luckBoosts = getgenv().SelectedEasterLuckBoosts or {}
+
+                    for boostType, enabled in pairs(luckBoosts) do
+                        if enabled then
+                            for _, item in pairs(inv) do
+                                local internalKey = tokenMapping[item.id]
+                                -- Оставляем запас 5000 токенов
+                                if internalKey and (item._am or 0) > 5000 then
+                                    local amountToUse = item._am - 5000
+                                    Net.Invoke("Easter2026ChanceMachine_AddTime", boostType, internalKey, amountToUse)
+                                    task.wait(0.1)
+                                end
                             end
                         end
                     end
